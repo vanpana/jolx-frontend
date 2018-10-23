b517d4dcedf65d331d2da78c7081aa963e592172
Claudia Pop <popclaudia97@gmail.com>
2018-10-23 13:40:33 +0300

    card structure

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import { configure } from '@storybook/angular';
 
 function loadStories() {
   require('../src/stories/index.ts');
+  require('../src/stories/card.component.ts');
 }
 
 configure(loadStories, module);

1bc241bc7534d6a7164cab16bd6d84d35cbf5013
Claudia Pop <popclaudia97@gmail.com>
2018-10-23 18:01:47 +0300

    jobcartlist almost done

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardListComponent implements OnInit {
 
+  range: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+
   constructor() { }
 
   ngOnInit() {

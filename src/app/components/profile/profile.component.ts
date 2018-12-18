119ebbcf6408a938d09613b62263555a3ee2d0d7
Teo <teodor_paius@yahoo.com>
2018-12-18 17:52:08 +0200

    Skills visible but ugly

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -5,6 +5,7 @@ import {PostingsService} from '../../services/postings.service';
 import {PostingsUpdated} from '../../models/message-bus-events/postings-updated';
 import {MessageBus} from '../../services/message-bus';
 import {UserPostingsUpdated} from '../../models/message-bus-events/user-postings-updated';
+import {Skill} from '../../models/skill';
 
 @Component({
   selector: 'app-profile',
@@ -13,11 +14,13 @@ import {UserPostingsUpdated} from '../../models/message-bus-events/user-postings
 })
 export class ProfileComponent implements OnInit {
   public postings: Posting[];
+  public skills: Skill[];
   constructor(public authService: AuthService,
               private postingsService: PostingsService,
               private messageBus: MessageBus
               ) {
     postingsService.fetchPostings();
+    this.skills = this.authService.user.skills;
     this.messageBus.observe(new UserPostingsUpdated(), (postingsUpdated) => {
       this.postings = postingsUpdated.postings;
     });

e1b78aaf44877b867f6045ca982a4250a1020a45
Vele George <georgevele2016@gmail.com>
2019-01-17 16:29:34 +0200

    Add RatingDialog and reviews showing for users

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -5,7 +5,6 @@ import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 import {filter} from 'rxjs/operators';
 import {Posting} from '../../models/posting';
 import {Skill} from '../../models/skill';
-import {interval} from 'rxjs/observable/interval';
 
 @Component({
   selector: 'app-user-profile',
@@ -27,7 +26,6 @@ export class UserProfileComponent implements OnInit {
   }
 
   ngOnInit() {
-    interval(0.5 * 1000).subscribe(() => this.updateReviews());
   }
 
   setUserFromUrl() {
@@ -39,30 +37,11 @@ export class UserProfileComponent implements OnInit {
       });
   }
 
-  updateReviews() {
-    if (this.user !== undefined) {
-      this.userService.read(this.user.id).subscribe(
-        user => {
-          user.reviewsReceived.forEach(review => {
-            if (this.user.reviewsReceived.filter(r => r._id === review._id).length === 0) {
-              this.user.reviewsReceived.push(review);
-            }
-          });
-
-          this.user.reviewsReceived.forEach(review => {
-            if (user.reviewsReceived.filter(r => r._id === review._id).length === 0) {
-              this.user.reviewsReceived.splice(this.user.reviewsReceived.indexOf(review), 1);
-            }
-          });
-        }
-      );
-    }
-  }
-
   getUser(id) {
     this.userService.read(id).subscribe(
       user => {
         this.user = user;
+        console.log(user);
       }
     );
   }
